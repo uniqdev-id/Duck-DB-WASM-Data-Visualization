d012fbdea571683dbbccaccd10c4944a0b3cac18
AnnasBlackHat <annas685@gmail.com>
2026-06-26 14:39:21 +0700

    update iframe

--- components/IframeHeightMessenger.tsx
+++ components/IframeHeightMessenger.tsx
@@ -4,32 +4,22 @@ import { useEffect } from "react";
 
 const PARENT_ORIGIN = "https://pos.uniq.web.id";
 
-function updateHeight() {
-  if (typeof window === "undefined") {
-    return;
-  }
-
-  window.parent.postMessage(
-    {
-      type: "iframe-height",
-      height: document.documentElement.scrollHeight,
-    },
-    PARENT_ORIGIN,
-  );
-}
-
 export function IframeHeightMessenger() {
   useEffect(() => {
-    updateHeight();
+    const resizeObserver = new ResizeObserver(() => {
+      window.parent.postMessage(
+        {
+          type: "iframe-height",
+          height: document.documentElement.scrollHeight,
+        },
+        PARENT_ORIGIN,
+      );
+    });
 
-    const observer = new ResizeObserver(updateHeight);
-    observer.observe(document.body);
-
-    window.addEventListener("load", updateHeight);
+    resizeObserver.observe(document.body);
 
     return () => {
-      observer.disconnect();
-      window.removeEventListener("load", updateHeight);
+      resizeObserver.disconnect();
     };
   }, []);
 
